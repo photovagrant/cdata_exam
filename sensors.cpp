e3e7d657a095e710d5d5cc1f03dc917e8cd05ca7
photovagrant <photovagrant.pda@gmail.com>
2013-09-07 16:20:19 +0800

    [EDIT] add operation action = initial.

--- sensors.cpp
+++ sensors.cpp
@@ -3,6 +3,30 @@ https://android.googlesource.com/device/samsung/tuna/+/android-4.3_r2.2/libsenso
 */
 
 
+
+static int poll__activate(struct sensors_poll_device_t *dev,
+                          int handle, int enabled)
+{
+
+	// 1. initial
+	ioctl(handle,SENSOR_INIT,0);
+	// 2. 'enable'  . enable or disable ? what's is it ?
+        //  if (enable == 0 ) {
+	//  }
+	//  else
+	//  {
+	// }		
+	if (enable == 0)
+	{
+	//ioctl(handle,SENSOR_INIT,0);
+	}
+	else
+	{
+	ioctl(handle,SENSOR_INIT,0);
+	}
+	// 3. 'handle' ? => open
+}
+
 static struct hw_module_methods_t sensors_module_methods = { //it's a class in C++
         open: open_sensors
 };
@@ -23,20 +47,22 @@ struct sensors_module_t HAL_MODULE_INFO_SYM = {  //CLASS !!!
         get_sensors_list: sensors__get_sensors_list,
 };
 
-tatic int open_sensors(const struct hw_module_t* module, const char* id,
+
+
+/** Open a new instance of a sensor device using name */
+static int open_sensors(const struct hw_module_t* module, const char* id,
                         struct hw_device_t** device)
 {
     FUNC_LOG;
     int status = -EINVAL;
     sensors_poll_context_t *dev = new sensors_poll_context_t();
-
     memset(&dev->device, 0, sizeof(sensors_poll_device_t));
 
-    dev->device.common.tag = HARDWARE_DEVICE_TAG;
+    dev->device.common.tag = HARDWARE_DEVICE_TAG; //operation ...
     dev->device.common.version  = 0;
     dev->device.common.module   = const_cast<hw_module_t*>(module);
     dev->device.common.close    = poll__close;
-    dev->device.activate        = poll__activate;
+    dev->device.activate        = poll__activate; //init
     dev->device.setDelay        = poll__setDelay;
     dev->device.poll            = poll__poll;
 
